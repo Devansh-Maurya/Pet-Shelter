ca6d74039e6a39bc377205f1f8361f3db9b50587
Devansh Maurya <Devansh233@gmail.com>
2018-08-18 15:57:41 +0530

    Added contract class

--- app/src/main/java/com/example/android/myapplication/data/PetContract.java
+++ app/src/main/java/com/example/android/myapplication/data/PetContract.java
@@ -23,7 +23,7 @@ public final class PetContract {
 
         public static final int GENDER_UNKNOWN = 0;
         public static final int GENDER_MALE = 1;
-        public static final int GENDER__FEMALE = 2;
+        public static final int GENDER_FEMALE = 2;
     }
 
 }
