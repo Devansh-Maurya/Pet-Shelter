19a615264fbd8738267d67eb2e1934bde0e4f6c8
Devansh Maurya <Devansh233@gmail.com>
2018-08-18 15:56:37 +0530

    Added contract class

---
+++ app/src/main/java/com/example/android/myapplication/data/PetContract.java
@@ -0,0 +1,29 @@
+package com.example.android.myapplication.data;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by devansh on 18/8/18.
+ */
+
+public final class PetContract {
+
+    private PetContract() {
+    }
+
+    public static final class PetEntry implements BaseColumns {
+
+        public static final String TABLE_NAME = "pets";
+
+        public static final String _ID = BaseColumns._ID;
+        public static final String COLUMN_PET_NAME = "name";
+        public static final String COLUMN_PET_BREED = "breed";
+        public static final String COLUMN_PET_GENDER = "gender";
+        public static final String COLUMN_PET_WEIGHT = "weight";
+
+        public static final int GENDER_UNKNOWN = 0;
+        public static final int GENDER_MALE = 1;
+        public static final int GENDER__FEMALE = 2;
+    }
+
+}
